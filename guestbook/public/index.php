c629197ef077fa99d8bddda8ddbab3d1f32a850a
nicoyamin <nicoyamin@hotmail.com.ar>
2016-10-05 18:48:55 -0300

    database saving working with comments table

--- guestbook/public/index.php
+++ guestbook/public/index.php
@@ -4,6 +4,13 @@ $database = new medoo([
     'database_type' => 'sqlite',
 'database_file' => '../storage/database.db'
 ]);
+
+$comment = new SitePoint\Comment($database);
+$comment->setEmail('bruno@skvorc.me')
+    ->setName('Bruno Skvorc')
+    ->setComment('Hooray! Saving comments works!')
+    ->save();
+dump($database->error());
 ?>
 
 <!doctype html>
